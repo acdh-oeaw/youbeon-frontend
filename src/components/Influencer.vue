6c7ad4e108371f007426cc0d05848950ae616205
Markus Hasenberger <markus@hasenberger.at>
2021-07-02 10:33:57 +0200

    start if the influencer page

--- src/components/Influencer.vue
+++ src/components/Influencer.vue
@@ -1,36 +1,94 @@
 <template>
   <vContainer>
-    <h1>Influencer</h1>
-    <p></p>
+    <v-card class="sticky-card mt-2">
+      <v-row no-gutters>
+        <v-col class="pa-0 flex-grow-1">
+          <v-autocomplete
+            item-text="bezeichnung"
+            item-value="id"
+            clearable
+            deletable-chips
+            solo
+            text
+            flat
+            hide-details
+            label="Suche..."
+          >
+          </v-autocomplete>
+        </v-col>
+        <div class="vl"></div>
+        <v-col class="pa-0 ma-0" cols="auto">
+          <v-menu max-height="80vh" offset-y :close-on-content-click="false">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                class="mx-1"
+                style="margin-top:5px"
+                text
+                v-on="on"
+                v-bind="attrs"
+              >
+                test
+                <v-icon>mdi-menu-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list dense>
+              <v-list-item
+                dense
+                v-bind:key="item.id"
+                v-for="item in dropDownItems"
+              >
+                <v-list-item-title>item.title</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-col>
+      </v-row>
+    </v-card>
+    <d3-network
+      id="network"
+      :net-nodes="nodes"
+      :net-links="links"
+      :options="options"
+    >
+    </d3-network>
   </vContainer>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
+// eslint-disable-next-line
+import D3Network from "vue-d3-network";
 
 @Component({
-  components: {},
-  name: 'Influencer',
+  components: {
+    D3Network,
+  },
+  name: "Influencer",
 })
 export default class Influencer extends Vue {
-
+  dropDownItems: string[] = [];
+  nodes: any = [];
+  links: any = [];
+  options = {
+    force: 2000,
+    nodeLabels: true,
+  };
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="vue-d3-network/dist/vue-d3-network.css"></style>
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+#network {
+  margin-top: 5vh;
+  border: 2px solid #b0dcd9;
+  background-color: #e5e5e5;
+  height: 70vh;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.vl {
+  border-left: 2px solid #e5e5e5;
+  height: 30px;
+  margin-top: 7px;
 }
 </style>
