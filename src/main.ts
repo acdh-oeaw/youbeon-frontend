16f295dd39a4f013fc7540e3851b690f7940ea9b
Stefan Probst <stefanprobst@posteo.de>
2022-09-15 19:47:28 +0200

    chore: remove unused vuex dependency

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from '@/plugins/vuetify'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "@/plugins/vuetify";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
