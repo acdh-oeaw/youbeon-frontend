7997cdeebfc33e60df9e61baf82cfb36d8428606
Stefan Probst <stefanprobst@posteo.de>
2022-09-15 23:24:12 +0200

    build: generate favicons and basic meta tags

--- vue.config.mjs
+++ vue.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from '@vue/cli-service'
 
+import { metadata } from './config/metadata.config.mjs'
+
 const config = defineConfig({
   chainWebpack(config) {
     /**
      * @see https://github.com/vuejs/vue-cli/issues/3123
      */
     config.resolve.alias.set('~', process.cwd())
+
+    config.plugin('html').tap((args) => {
+      args[0].title = metadata.title
+      args[0].meta = {
+        description: metadata.description,
+        'og:title': metadata.title,
+        'og:description': metadata.description,
+      }
+      return args
+    })
   },
   productionSourceMap: false,
   transpileDependencies: true,
