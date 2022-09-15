b154e7fb9055e2a857287cf0597e805bb8fcfdbb
Stefan Probst <stefanprobst@posteo.de>
2022-09-15 22:19:08 +0200

    chore: add path alias for root

--- vue.config.mjs
+++ vue.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from '@vue/cli-service'
 
 const config = defineConfig({
+  chainWebpack(config) {
+    /**
+     * @see https://github.com/vuejs/vue-cli/issues/3123
+     */
+    config.resolve.alias.set('~', process.cwd())
+  },
+  productionSourceMap: false,
   transpileDependencies: true,
 })
 
