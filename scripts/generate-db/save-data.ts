47e1efe6c40a144cf8977956747a8615ffb02e79
Stefan Probst <stefanprobst@posteo.de>
2023-01-23 10:23:04 +0100

    fix: sort resources

--- scripts/generate-db/save-data.ts
+++ scripts/generate-db/save-data.ts
@@ -10,13 +10,19 @@ export async function saveData(data: TransformedData): Promise<void> {
 	for (const [key, value] of Object.entries(data)) {
 		const filePath = join(process.cwd(), "src", "db", key + ".ts");
 
+		const sorted = new Map(
+			[...value].sort(([, a], [, z]) => {
+				return a.label.localeCompare(z.label);
+			}),
+		);
+
 		/**
 		 * We cannot use a `reviver` for `Map` and `Set` when importing json client-side,
 		 * so we dump to javascript, not json.
 		 */
 		await writeFile(
 			filePath,
-			format(`export const ${key} = ${serialize(value)}`, { parser: "typescript" }),
+			format(`export const ${key} = ${serialize(sorted)}`, { parser: "typescript" }),
 		);
 	}
 }
