ff211e584ef13d8f9f1b99c190556b402dca5213
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2025-06-12 10:14:52 +0000

    refactor: update landing page
    see internal redmine issue 25204
    minor layout updates

--- src/components/project-section.vue
+++ src/components/project-section.vue
@@ -6,60 +6,76 @@ import HomeDisclosure from "./home-disclosure.vue";
 	<section>
 		<div class="mx-auto max-w-5xl p-8">
 			<HomeDisclosure title="Das Projekt">
-				<p>Projektdauer: Sept 2020-März 2023</p>
-				<p>
-					Institutionelle Anbindung: Institut für Stadt- und Regionalforschung der ÖAW (<a
-						href="https://www.oeaw.ac.at/isr/home"
-						>https://www.oeaw.ac.at/isr/home</a
-					>), Forschungszentrum Religion and Transformation… der Universität Wien (<a
-						href="https://www.religionandtransformation.at/"
-						>https://www.religionandtransformation.at/</a
-					>) Fördergeber:
-					<a
-						href="https://web.archive.org/web/20240227101533/https:/www.oeaw.ac.at/foerderungen/innovationsfonds-forschung-wissenschaft-und-gesellschaft"
-						>Innovationsfonds Forschung, Wissenschaft und Gesellschaft der Österreichischen Akademie
-						der Wissenschaften</a
-					>
-				</p>
-				<p>
-					YouBeOn ist ein exploratives Forschungsprojekt, das Zugehörigkeitsverhandlungen religiöser
-					junger Menschen aus städtischem Umfeld in on- und offline Räumen untersucht. Obwohl in
-					europäischen Gesellschaften ein zunehmendes Bewusstsein um wachsende Diversität zu
-					beobachten ist, gibt es immer wieder Versuche, dieser Superdiversität eindimensionale
-					Ideen kollektiver Identität entgegenzusetzen. Ganz anders gestalten sich die
-					Lebensrealitäten junger Menschen, die im superdiversen Alltag europäischer Städte
-					aufwachsen und ihre multiplen Zugehörigkeiten flexibel verhandeln. Der narrative Prozess,
-					indem solche Verhandlungen stattfinden, nimmt viele Formen an und findet dabei zunehmen in
-					entgrenzten digitalen Räumen statt. Obwohl online Aktivitäten eng an offline Aktivitäten
-					geknüpft sind, werden sie bisher in der Forschung meist getrennt wahrgenommen und häufig
-					losgelöst von der wachsenden Politisierung (religiöser) Diversität betrachtet.
-				</p>
-				<p>
-					YouBeOn untersucht on- und offline Identifikationsprozesse und fragt danach wie
-					selbstidentifizierte religiöse junge Menschen in der superdiversen Stadt multiple
-					Zugehörigkeiten verhandeln. Dabei bauen wir auf das wachsende wissenschaftliche und
-					öffentliche Interesse an <em>Belonging</em> (Zugehörigkeit), sowohl im Hinblick auf
-					Religionszugehörigkeit, als auch in Bezug auf individuelle Zugehörigkeitspolitiken. Wir
-					untersuchen religiöse junge Menschen, da das Zusammentreffen der Differenzkategorien
-					Religion, Alter und race Identifikationsprozesse herausfordert. Anstatt
-					Identitätszuschreibungen als gegeben hinzunehmen werden multiple Zugehörigkeiten bewusster
-					wahrgenommen und in einen Dialog gebracht.
-				</p>
-				<p>
-					YouBeOn möchte zur Weiterentwicklung und theoretischen Ausarbeitung des
-					Belonging-Konzeptes beitragen und nutzt dafür einen innovativen methodischen Zugang. Durch
-					partizipative Methoden der qualitativen Sozialforschung werden jene junge Menschen, die
-					wir untersuchen, aktiv in den Forschungsprozess miteingebunden und als Akteur*innen im
-					Projekt wahrgenommen. Gemeinsam mit ihnen wird das Forscher*innen-Team Interviewdaten und
-					Inhalte sozialer Netzwerke analysieren und interpretieren. Ergänzt werden diese Daten
-					durch Gruppeninterviews mit TeilnehmerInnnen. Gemeinsam mit Expert*innen des „Austrian
-					Center for Digital Humanities“ entwickeln wir eine online frei zugängliche Applikation,
-					die die gesammelten Daten visualisiert. In der YouBeOn Map wird es möglich sein,
-					Verbindungen zwischen städtischen Lebensräumen und Identifikationspunkten online zu
-					erkunden. Dadurch kann ein Verständnis von
-					<em>Belonging</em> entwickelt werden, das der Komplexität vielfältiger Zugehörigkeiten
-					religiöser junger Menschen in der Stadt Rechnung trägt.
-				</p>
+				<section>
+					<h3 class="font-display text-base">Projektdauer</h3>
+					<p>September 2020 bis März 2023</p>
+				</section>
+				<section class="mt-4">
+					<h3 class="font-display text-base">Institutionelle Anbindung</h3>
+					<p>
+						<a class="underline decoration-dotted" href="https://www.oeaw.ac.at/isr/home"
+							>Institut für Stadt- und Regionalforschung der ÖAW</a
+						>,
+						<a class="underline decoration-dotted" href="https://www.religionandtransformation.at/"
+							>Forschungszentrum Religion and Transformation in Contemporary Society der Universität
+							Wien</a
+						>
+					</p>
+				</section>
+				<section class="mt-4">
+					<h3 class="font-display text-base">Fördergeber</h3>
+					<p>
+						<a
+							class="underline decoration-dotted"
+							href="https://web.archive.org/web/20240227101533/https:/www.oeaw.ac.at/foerderungen/innovationsfonds-forschung-wissenschaft-und-gesellschaft"
+							>Innovationsfonds Forschung, Wissenschaft und Gesellschaft der Österreichischen
+							Akademie der Wissenschaften</a
+						>
+					</p>
+				</section>
+				<section class="mt-8">
+					<h3 class="sr-only">Projektbeschreibung</h3>
+					<p>
+						YouBeOn ist ein exploratives Forschungsprojekt, das Zugehörigkeitsverhandlungen
+						religiöser junger Menschen aus städtischem Umfeld in on- und offline Räumen untersucht.
+						Obwohl in europäischen Gesellschaften ein zunehmendes Bewusstsein um wachsende
+						Diversität zu beobachten ist, gibt es immer wieder Versuche, dieser Superdiversität
+						eindimensionale Ideen kollektiver Identität entgegenzusetzen. Ganz anders gestalten sich
+						die Lebensrealitäten junger Menschen, die im superdiversen Alltag europäischer Städte
+						aufwachsen und ihre multiplen Zugehörigkeiten flexibel verhandeln. Der narrative
+						Prozess, indem solche Verhandlungen stattfinden, nimmt viele Formen an und findet dabei
+						zunehmen in entgrenzten digitalen Räumen statt. Obwohl online Aktivitäten eng an offline
+						Aktivitäten geknüpft sind, werden sie bisher in der Forschung meist getrennt
+						wahrgenommen und häufig losgelöst von der wachsenden Politisierung (religiöser)
+						Diversität betrachtet.
+					</p>
+					<p>
+						YouBeOn untersucht on- und offline Identifikationsprozesse und fragt danach wie
+						selbstidentifizierte religiöse junge Menschen in der superdiversen Stadt multiple
+						Zugehörigkeiten verhandeln. Dabei bauen wir auf das wachsende wissenschaftliche und
+						öffentliche Interesse an <em>Belonging</em> (Zugehörigkeit), sowohl im Hinblick auf
+						Religionszugehörigkeit, als auch in Bezug auf individuelle Zugehörigkeitspolitiken. Wir
+						untersuchen religiöse junge Menschen, da das Zusammentreffen der Differenzkategorien
+						Religion, Alter und race Identifikationsprozesse herausfordert. Anstatt
+						Identitätszuschreibungen als gegeben hinzunehmen werden multiple Zugehörigkeiten
+						bewusster wahrgenommen und in einen Dialog gebracht.
+					</p>
+					<p>
+						YouBeOn möchte zur Weiterentwicklung und theoretischen Ausarbeitung des
+						Belonging-Konzeptes beitragen und nutzt dafür einen innovativen methodischen Zugang.
+						Durch partizipative Methoden der qualitativen Sozialforschung werden jene junge
+						Menschen, die wir untersuchen, aktiv in den Forschungsprozess miteingebunden und als
+						Akteur*innen im Projekt wahrgenommen. Gemeinsam mit ihnen wird das Forscher*innen-Team
+						Interviewdaten und Inhalte sozialer Netzwerke analysieren und interpretieren. Ergänzt
+						werden diese Daten durch Gruppeninterviews mit TeilnehmerInnnen. Gemeinsam mit
+						Expert*innen des „Austrian Center for Digital Humanities“ entwickeln wir eine online
+						frei zugängliche Applikation, die die gesammelten Daten visualisiert. In der YouBeOn Map
+						wird es möglich sein, Verbindungen zwischen städtischen Lebensräumen und
+						Identifikationspunkten online zu erkunden. Dadurch kann ein Verständnis von
+						<em>Belonging</em> entwickelt werden, das der Komplexität vielfältiger Zugehörigkeiten
+						religiöser junger Menschen in der Stadt Rechnung trägt.
+					</p>
+				</section>
 			</HomeDisclosure>
 		</div>
 	</section>
---
+++ src/components/vita.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const _props = defineProps<{
+	name: string;
+	pid: string;
+	image: string;
+	bio: string;
+}>();
+
+const imageUrl = computed(() => {
+	return new URL(`/src/assets/images/${_props.image}`, import.meta.url).href;
+});
+</script>
+
+<template>
+	<div class="flex flex-col items-center md:flex-row">
+		<img alt="" class="size-60 flex-shrink-0" :src="imageUrl" />
+		<div class="flex flex-col gap-2">
+			<h3 class="font-display text-base sm:text-lg">{{ name }}</h3>
+			<p>
+				(<a :href="pid">{{ pid }}</a
+				>)
+				<br />
+				<br />
+				{{ bio }}
+			</p>
+		</div>
+	</div>
+</template>
